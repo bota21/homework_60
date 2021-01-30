b073d5e18862a1eb82a191b66f83f0ef921e6eb6
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-01-30 10:56:43 +0600

    #2 Добавлены имена стран по API для компонента CountryList

--- src/components/CountryList/CountryList.js
+++ src/components/CountryList/CountryList.js
@@ -1,12 +1,20 @@
 import './CountryList.css';
 import { React } from 'react';
 
-const CountryList = props => {
+const CountryList = ({array}) => {
+    
+    let addCountryLists = array.map(item => {        
+        return <h5 tabIndex='1' className='countryName' key={item.alpha3Code} 
+        >
+        {item.name}
+        </h5>        
+    })
+    
     return (
         <div className='CountryList'>
-
+        {addCountryLists}
         </div>
-    )
-}
-
-export default CountryList
+        )
+    }
+    
+    export default CountryList
--- src/container/Main.js
+++ src/container/Main.js
@@ -1,11 +1,25 @@
 import './Main.css';
 import CountryList from '../components/CountryList/CountryList';
 import CountryInfo from '../components/CountryInfo/CountryInfo';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Main = () => {
+  const url = 'https://restcountries.eu/rest/v2/all';
+  const [countries, setCountries] =useState([]);
+  // console.log(countries);
+  useEffect(() => {
+axios.get(url)
+.then(res => {
+  console.log(res.data);
+  setCountries(res.data)
+})
+.catch(error => console.error)
+  }, [])
+
   return (
     <div className="main">
-<CountryList />
+<CountryList array={countries}/>
 <CountryInfo />
     </div>
   );
